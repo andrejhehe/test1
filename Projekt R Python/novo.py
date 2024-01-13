f1a04a060493095dfaa57d5489b079ec8ce4c91d
Andrej Stanković <as53991@fer.hr>
2024-01-14 00:09:43 +0100

    Radi ovo ali onako

--- Projekt R Python/novo.py
+++ Projekt R Python/novo.py
@@ -4,60 +4,51 @@ from matplotlib.animation import FuncAnimation
 import random
 import time
 import scipy as sp
+from IPython.display import display, clear_output
 
-animation_has_run = False
 primjer_staze_i_dodani_flow = ['A', 'B', 'D', 'F', 3]
 
 checked_paths = [0, 0, 0, 0]
-
-def init():
-        #do nothing
-        pass
 
 paths_list = [['A', 'B', 'D', 'F', 3],
             ['A', 'C', 'E', 'F', 2],
             ['A', 'C', 'G', 'F', 3],
             ['A', 'B', 'E', 'F', 10]]
-            
 
 
 
 def update_flow(G, primjer_staze_i_dodani_flow):
     # Simulate updating the flow values (you can replace this logic)
-
-    print("Prvi red u update_flow funkciji\n")
     for i in range(len(primjer_staze_i_dodani_flow) - 1):
         u, v = primjer_staze_i_dodani_flow[i], primjer_staze_i_dodani_flow[i + 1]
         if G.has_edge(u, v):
             G[u][v]['flow'] += primjer_staze_i_dodani_flow[-1]
-    
-    print("Zadnji dio funkcije update_flow\n")
 
 def animate_flow_update(frame, G, pos, ax, paths_lists):
-    
     for i in range(len(paths_list)):
-        update_flow(G, paths_lists[i])
-        print("Red poslije poziva funkcije update_flow\n")
-        
-        # Clear the previous plot
-        ax.clear()
-        print("ax.clear")
+        if checked_paths[i] == 0:
+            update_flow(G, paths_lists[i])
+            checked_paths[i] = 1
+            
+            # Clear the previous plot
+            ax.clear()
 
-        # Draw the directed graph with updated labels
-        nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=700, node_color=node_colors, font_color='black', font_size=10, edge_color='gray', width=1.5, alpha=0.7, arrowsize=15)
-        print("nx.draw")
+            # Draw the directed graph with updated labels
+            nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=700, node_color=node_colors, font_color='black', font_size=10, edge_color='gray', width=1.5, alpha=0.7, arrowsize=15)
 
-        # Add updated edge labels with flow/capacity information at the top of the edges
-        edge_labels = {(u, v): f"{G[u][v]['flow']}/{G[u][v]['capacity']}" for u, v in G.edges()}
-        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red', bbox=dict(boxstyle="round,pad=0.3", edgecolor="none", facecolor="none"), verticalalignment="bottom")
-        print("nx.draw_network_edge_labels")
+            # Add updated edge labels with flow/capacity information at the top of the edges
+            edge_labels = {(u, v): f"{G[u][v]['flow']}/{G[u][v]['capacity']}" for u, v in G.edges()}
+            nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red', bbox=dict(boxstyle="round,pad=0.3", edgecolor="none", facecolor="none"), verticalalignment="bottom")
 
-        # Show the plot
-        plt.pause(2)
-        print("plt.pause")
+            # Display the plot interactively
+            display(fig)
+            clear_output(wait=True)
+            
+            # Pause for a short time (adjust as needed)
+            time.sleep(0.2)
 
-    
-
+    # Pause for a longer time after the loop (adjust as needed)
+    time.sleep(2)
 
 
 
@@ -76,10 +67,8 @@ G = nx.DiGraph()
 for i in range(len(graph_matrix)):
     slovo_vrh = chr(65 + i)
     G.add_node(slovo_vrh)
-    print("Dodajem novi vrh " + chr(65 + i)   +"\n")
 
 # end for
-print("\n")
 
 #pocetni graf sa tokom 0 u svim bridovima
 for i in range(len(graph_matrix)):
@@ -106,21 +95,6 @@ pos = nx.spectral_layout(G, center=None)
 
 # Set up the animation
 fig, ax = plt.subplots()
+ani = FuncAnimation(fig, animate_flow_update, fargs=(G, pos, ax, paths_list),  interval=2000)  # Update every 2 seconds
 
-print("Red prije pozivanja animate_flow_update funckije\n")
-
-
-ani = FuncAnimation(fig, animate_flow_update, fargs=(G, pos, ax, paths_list), init_func=init,  frames=1, interval=1000, repeat = False) 
-
-'''ani1 = FuncAnimation(fig, animate_flow_update, fargs=(G, pos, ax, paths_list[1]), init_func=init,  frames=1, interval=1000, repeat = False) 
-plt.show()
-
-ani2 = FuncAnimation(fig, animate_flow_update, fargs=(G, pos, ax, paths_list[2]), init_func=init,  frames=1, interval=1000, repeat = False) 
-plt.show()
-
-ani3 = FuncAnimation(fig, animate_flow_update, fargs=(G, pos, ax, paths_list[3]), init_func=init,  frames=1, interval=1000, repeat = False) 
-plt.show()'''
-
-
-print("Ovo je kraj")
 plt.show()
